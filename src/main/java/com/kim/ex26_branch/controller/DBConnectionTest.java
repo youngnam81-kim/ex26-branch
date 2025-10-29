dd2c38622b38986d3d61adc2bd5726c7cace55b3
youngnam81-kim <kimyn81@naver.com>
2025-10-29 11:12:17 +0900

    db conn 브렌치에서 수정

--- src/main/java/com/kim/ex26_branch/controller/DBConnectionTest.java
+++ src/main/java/com/kim/ex26_branch/controller/DBConnectionTest.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DBConnectionTest {
-
+	// dbconn db
 	private DataSource dataSource;
 
     public DBConnectionTest(DataSource dataSource) {
